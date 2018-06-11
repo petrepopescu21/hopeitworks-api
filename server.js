edb72a800a5cdbb210f4dc31eeab5376529f5b6d
Petre Popescu <petre.popescu@microsoft.com>
2018-06-11 12:40:27 +0300

    added file upload

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
+require('dotenv').config()
+var cors = require('cors')
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var multer = require('multer')
+var MulterAzureStorage = require('multer-azure-storage')
+var upload = multer({
+    storage: new MulterAzureStorage({
+        azureStorageConnectionString: process.env.IMAGE_STORAGE,
+        containerName: 'photos',
+        containerSecurity: 'blob'
+    })
+})
+
+app.use(cors())
 
 app.get('/', function(req, res){
     res.send("Hello World!")
 });
 
+app.post('/upload', upload.single('photos'), function(req,res){
+    console.log(req.body)
+    io.emit('imageMessage',{
+        message: req.file.url,
+        user_name: req.body.user_name,
+        id: req.body.user_id,
+        type: "image",
+        timestamp: new Date()
+    })
+    res.send('OK')
+})
+
 io.on('connection', function(socket){
     let name = socket.handshake.query.name
-    console.log('a user connected ' + name);
     io.emit('conn', {name:name, time:new Date()})
 
     socket.on('message',(data)=>{
-        console.log(data)
         socket.broadcast.emit('message',data)
     })
 
+    socket.on('image',(data)=>{
+        console.log(data)
+        io.emit('image',data)
+    })
+
     socket.on('disconnect',(reason)=>{
-        console.log('a user disconnected ' + name)
         io.emit('disconn', {name:name, time:new Date()})
     })
 });
